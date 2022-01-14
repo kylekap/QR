f64e98ce38fb902f70201b7d09b2bc82b95a64f5
kylekap <kylekaptbone@yahoo.com>
2022-01-13 19:38:29 -0500

    Creation of template

---
+++ Project/core.py
@@ -0,0 +1,11 @@
+#https://realpython.com/python-pep8/
+#https://docs.python-guide.org/writing/structure/
+
+def main():
+    """main function used to run the program
+    """
+
+if __name__ == '__main__':
+    """[summary]
+    """    
+    main()
